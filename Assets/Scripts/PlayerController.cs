1e69ab42b9aac3641aa84ebb1faf16c5863e2dbe
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-06-06 15:08:19 +0300

    vähän grafiikkaa yms, päivä 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D body;
     // input
     private float horizontalMovement;
 
@@ -11,10 +12,16 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed = 5f;
     private Vector2 movement = new Vector2();
 
+    // jumping
+    public float jumpForce = 1f;
+
+    // animation
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,10 +30,20 @@ public class PlayerController : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         //Debug.Log(horizontalMovement);
         movement.x = horizontalMovement * moveSpeed;
+
+        // animation
+        animator.SetFloat("speed", Mathf.Abs(horizontalMovement));
     }
 
     void FixedUpdate()
     {
+        // moving
         transform.Translate(movement * Time.deltaTime);
+
+        // jumping
+        if (Input.GetButton("Jump"))
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
